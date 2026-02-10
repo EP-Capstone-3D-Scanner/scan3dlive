c93260a11ab95b0b78a8699abea1ce2eba7661f4
hsdiep <hsdiep@ualberta.ca>
2026-02-10 00:57:36 +0000

    Added a usb_cam_formatter which crops out the left of the zed camera image.

---
+++ _check_lidar.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/tmp/scan3dlive_install_files/build/Livox-SDK/sample/lidar/lidar_sample
---
+++ usb_cam_ws/src/usb_cam_formatter/src/usb_cam_formatter.cpp
@@ -0,0 +1,52 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "cv_bridge/cv_bridge.h"
+#include "opencv2/opencv.hpp"
+
+class ImageCropper : public rclcpp::Node {
+public:
+  ImageCropper()
+  : Node("usb_cam_formatter")
+  {
+    // Subscriber
+    subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+      "/camera1/image_raw", 10, std::bind(&ImageCropper::image_callback, this, std::placeholders::_1));
+    
+    // Publisher
+    publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/camera1/image_cropped", 10);
+  }
+
+private:
+  void image_callback(const sensor_msgs::msg::Image::SharedPtr msg) const {
+    try {
+      // 1. Convert ROS Image to OpenCV Mat
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+
+      // 2. Define the Region of Interest (ROI)
+      // Rect(x, y, width, height)
+      int half_width = cv_ptr->image.cols / 2;
+      cv::Rect roi(half_width, 0, half_width, cv_ptr->image.rows);
+
+      // 3. Crop and Publish
+      cv::Mat cropped_img = cv_ptr->image(roi);
+      
+      auto out_msg = cv_bridge::CvImage(msg->header, msg->encoding, cropped_img).toImageMsg();
+      publisher_->publish(*out_msg);
+
+    } catch (cv_bridge::Exception& e) {
+      RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+    }
+  }
+
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+};
+
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ImageCropper>());
+  rclcpp::shutdown();
+  return 0;
+}

321dbd9557f4844bc8627caf894210ff0f5f0801
Your Name <you@example.com>
2026-02-12 00:05:02 +0000

    First implementation of fast-livo2, but lidar interface is not working.

--- _check_lidar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/tmp/scan3dlive_install_files/build/Livox-SDK/sample/lidar/lidar_sample
--- _test_scripts/check_lidar.sh
+++ _test_scripts/check_lidar.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-/tmp/scan3dlive_install_files/build/Livox-SDK2/samples/livox_lidar_quick_start/livox_lidar_quick_start $SCRIPT_DIR/check_lidar_config/hap_config.json
+/tmp/scan3dlive_install_files/build/Livox-SDK/sample/lidar/lidar_sample

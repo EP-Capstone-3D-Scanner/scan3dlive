b559fbd3ee6a5df71cccfeffcc382dff93a7bde6
hsdiep <hsdiep@ualberta.ca>
2026-02-23 17:53:13 +0000

    Added imu timebase change to system time.

--- livox_ros2_driver_ws/src/livox_ros2_driver_test/src/lidar_formatter.cpp
+++ livox_ros2_driver_ws/src/livox_ros2_driver_test/src/lidar_formatter.cpp
@@ -10,6 +10,8 @@
 
 #include "livox_ros_driver2/msg/custom_msg.hpp"
 
+#include "sensor_msgs/msg/imu.hpp"
+
 using std::placeholders::_1;
 
 class LidarFormatter : public rclcpp::Node
@@ -18,30 +20,31 @@ class LidarFormatter : public rclcpp::Node
     LidarFormatter()
     : Node("lidar_formatter")
     {
-      subscription_ = this->create_subscription<livox_interfaces::msg::CustomMsg>(
-      "livox/lidar", 10, std::bind(&LidarFormatter::topic_callback, this, _1));
+      subscription_lidar_ = this->create_subscription<livox_interfaces::msg::CustomMsg>(
+      "livox/lidar", 10, std::bind(&LidarFormatter::topic_callback_lidar, this, _1));
 
-      publisher_ = this->create_publisher<livox_ros_driver2::msg::CustomMsg>("livox/lidar_formatted", 10);
+      publisher_lidar_ = this->create_publisher<livox_ros_driver2::msg::CustomMsg>("livox/lidar_formatted", 10);
+      
+      subscription_imu_ = this->create_subscription<sensor_msgs::msg::Imu>(
+      "livox/imu", 10, std::bind(&LidarFormatter::topic_callback_imu, this, _1));
+
+      publisher_imu_ = this->create_publisher<sensor_msgs::msg::Imu>("livox/imu_formatted", 10);
     }
 
   private:
-    void topic_callback(const livox_interfaces::msg::CustomMsg & msg_in) const
+    void topic_callback_lidar(const livox_interfaces::msg::CustomMsg & msg_in) const
     {
       auto msg_out = livox_ros_driver2::msg::CustomMsg();
 
       auto current_time = this->now();
 
-      // 1. Copy Metadata
       msg_out.header = msg_in.header;
       msg_out.header.stamp = current_time;
       msg_out.timebase = current_time.nanoseconds(); //msg_in.timebase;
       msg_out.point_num = msg_in.point_num;
       msg_out.lidar_id = msg_in.lidar_id;
       msg_out.rsvd = msg_in.rsvd;
-
-      // 2. Efficiently transfer points
-      // If you don't need msg_in anymore, you can't move from a SharedPtr const& 
-      // easily, so we perform a vector assignment which is O(N).
+      
       msg_out.points.reserve(msg_in.points.size());
       
       for (const auto& p : msg_in.points) {
@@ -55,11 +58,26 @@ class LidarFormatter : public rclcpp::Node
           p_new.line = p.line;
           msg_out.points.push_back(p_new);
       }
-      publisher_->publish(msg_out);
+      publisher_lidar_->publish(msg_out);
       // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
     }
-    rclcpp::Subscription<livox_interfaces::msg::CustomMsg>::SharedPtr subscription_;
-    rclcpp::Publisher<livox_ros_driver2::msg::CustomMsg>::SharedPtr publisher_;
+
+    void topic_callback_imu(const sensor_msgs::msg::Imu & msg_in) const
+    {
+      auto msg_out = msg_in;
+
+      auto current_time = this->now();
+      msg_out.header.stamp = current_time;
+      
+      publisher_imu_->publish(msg_out);
+      // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
+
+    rclcpp::Subscription<livox_interfaces::msg::CustomMsg>::SharedPtr subscription_lidar_;
+    rclcpp::Publisher<livox_ros_driver2::msg::CustomMsg>::SharedPtr publisher_lidar_;
+    
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr subscription_imu_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr publisher_imu_;
 };
 
 int main(int argc, char * argv[])

90800901280089e3f1b8ef3007e3350685e4b9ad
hsdiep <hsdiep@ualberta.ca>
2026-03-09 23:12:19 +0000

    Adjusted fast_calib_test. Should be functional.

--- fast-calib_ws/src/fast_calib_test/launch/calib.launch.py
+++ fast-calib_ws/src/fast_calib_test/launch/calib.launch.py
@@ -7,6 +7,9 @@ from launch.conditions import IfCondition
 from launch.actions import SetEnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 
+import os
+import yaml
+
 def generate_launch_description():
     # Launch arguments
     rviz_arg = DeclareLaunchArgument(
@@ -20,14 +23,37 @@ def generate_launch_description():
     
     # Load parameters
     params_file = PathJoinSubstitution([pkg_share, 'config', 'qr_params.yaml'])
-    
+
+    # Load parameters to read from
+    yaml_file_path = os.path.join(
+        get_package_share_directory('fast_calib_test')
+        , 'config', 'qr_params.yaml')
+    with open(yaml_file_path, 'r') as f:
+        params_config = yaml.safe_load(f)
+    node_params = params_config['fast_calib']['ros__parameters']
+
+    # Load bag and image
+    bag_path = PathJoinSubstitution([pkg_share, 'calib_data', 
+        node_params.get('bag_path')])
+    image_path = PathJoinSubstitution([pkg_share, 'calib_data', 
+        node_params.get('image_path')])
+    output_path = PathJoinSubstitution([pkg_share, 
+        node_params.get('output_path')])
+
     # Fast calibration node
     fast_calib_node = Node(
         package='fast_calib',
         executable='fast_calib',
         name='fast_calib',
         output='screen',
-        parameters=[params_file]
+        parameters=[
+            params_file,
+            {
+                'bag_path': bag_path,
+                'image_path': image_path,
+                'output': output_path
+            }
+        ]
     )
     
     # RViz node
